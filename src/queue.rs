27d4f4a394141d0f1b4051d493e16e8782cd9064
Alex Chi <iskyzh@gmail.com>
2020-12-07 10:45:02 +0800

    server: add intel-queue-length in header (#21)

---
+++ src/queue.rs
@@ -0,0 +1,33 @@
+use std::io::Cursor;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::http::{ContentType, Method, Status};
+use rocket::{Data, Request, Response};
+
+use crate::common::IntelMission;
+
+pub struct QueueLength {
+    pub mission: IntelMission,
+}
+
+#[rocket::async_trait]
+impl Fairing for QueueLength {
+    fn info(&self) -> Info {
+        Info {
+            name: "Queue length in Header",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        // Rewrite the response to return the current counts.
+        if request.method() == Method::Get {
+            response.set_header(Header::new(
+                "X-Intel-Queue-Length",
+                self.mission.metrics.task_in_queue.get().to_string(),
+            ));
+        }
+    }
+}
